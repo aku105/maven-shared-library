01de6ad805b4d0268983f6327d7398d0bf97f213
Amit Kumar <akumar105@sapient.com>
2018-07-10 10:13:55 +0100

    Fixes

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -1,31 +1,11 @@
 #!/usr/bin/env groovy
 
 def call(String mavenVersion = 'maven:3.5.0-jdk-8', String mavenJavaOpts){
-    stage('Build') {
-      agent {
-        docker {
-          /*
-           * Reuse the workspace on the agent defined at top-level of Pipeline but run inside a container.
-           * In this case we are running a container with maven so we don't have to install specific versions
-           * of maven directly on the agent
-           */
-          reuseNode true
-          image '${mavenVersion}'
-        }
-      }
-      steps {
-        // using the Pipeline Maven plugin we can set maven configuration settings, publish test results, and annotate the Jenkins console
-        withMaven(
-          maven: "Maven3"
-          mavenOpts: '${mavenJavaOpts}'
-        ) {
-          sh 'mvn ${mavenArgs} clean package'
-        }
-      }
-      post {
-        success {
-          send_to_nexus()
-        }
-      }
-    }
+  // using the Pipeline Maven plugin we can set maven configuration settings, publish test results, and annotate the Jenkins console
+  withMaven(
+    maven: "Maven3"
+    mavenOpts: '${mavenJavaOpts}'
+  ) {
+    sh 'mvn ${mavenArgs} clean package'
+  }
 }
--- vars/mavenSonar.groovy
+++ vars/mavenSonar.groovy
@@ -1,20 +1,5 @@
 #!/usr/bin/env groovy
 
 def call(String mavenVersion = 'maven:3.5.0-jdk-8', String sonarCredential = 'sonar'){
-    stage('Sonar Scan') {
-          agent {
-            docker {
-              // we can use the same image and workspace as we did previously
-              reuseNode true
-              image '${mavenVersion}'
-            }
-          }
-          environment {
-            //use 'sonar' credentials scoped only to this stage
-            SONAR = credentials('${sonarCredential}')
-          }
-          steps {
-            sh 'mvn sonar:sonar -Dsonar.login=$SONAR_PSW'
-          }
-    }
+  sh 'mvn sonar:sonar -Dsonar.login=$SONAR_PSW'
 }
